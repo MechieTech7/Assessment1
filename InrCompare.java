ab1b609201ac691527918284c15e932cd8630b39
Lokesh Chandramurthy <79828815+MechieTech7@users.noreply.github.com>
2021-04-20 18:46:04 +0530

    Add files via upload

---
+++ InrCompare.java
@@ -0,0 +1,17 @@
+public class InrCompare {  
+    public static void main(String[] args) {          
+      int INR1 = 6700 ;  
+        int INR2 = 5500;  
+        int INR3 = 800;  
+        int INR4 = 9900;  
+         
+        System.out.println(Integer.compare(INR1, INR2));  
+       
+        System.out.println(Integer.compare(INR1, INR3));  
+      
+        System.out.println(Integer.compare(INR4, INR2));
+
+        
+          
+    }  
+}  

5646e825e43e30503bbe35cf2b5e540607500613
Lokesh Chandramurthy <79828815+MechieTech7@users.noreply.github.com>
2021-04-21 17:46:45 +0530

    Update InrCompare.java

--- InrCompare.java
+++ InrCompare.java
@@ -1,6 +1,6 @@
 package currency;
 import java.util.*;
-public class Currency {
+public class InrCompare {
 public static void main(String[] args){
 String arr[]={"INR 6700","INR 5500","INR 800","INR 9990"};
 List<Integer> lis=new ArrayList<Integer>();
